84eb43ca4251171a14e1f1c216f3af600187ad1d
Andrew Donley <andrewjdonley@gmail.com>
2013-09-12 21:51:51 -0700

    Initial Commit

---
+++ src/com/KeepingItSimpleApps/statscalculator/client/Stats_Calculator.java
@@ -0,0 +1,23 @@
+package com.KeepingItSimpleApps.statscalculator.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Stats_Calculator implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+	}
+}
